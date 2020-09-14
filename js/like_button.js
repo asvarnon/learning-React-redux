bc5b55fe464db34ad19bfc209237864ce2735c7a
Austin Varnon <austinvarnon@gmail.com>
2020-09-14 15:57:06 -0500

    commit

--- js/like_button.js
+++ js/like_button.js
@@ -1,28 +1,34 @@
 'use strict';
 
-const e = React.createElement;
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
+//~~~~~~~~~~~~~~~~~~~~~Like Button Example~~~~~~~~~~~~~~~~~~~~`
+// const e = React.createElement;
+//
+// class LikeButton extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { liked: false };
+//   }
+//
+//   render() {
+//     if (this.state.liked) {
+//       return 'You liked this.';
+//     }
+//
+//     return e(
+//       'button',
+//       { onClick: () => this.setState({ liked: true }) },
+//       'Like'
+//     );
+//   }
+// }
+//
+// const domContainer = document.querySelector('#like_button_container');
+// ReactDOM.render(e(LikeButton), domContainer);
 
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
+//~~~~~~~~~~~~~~~~~~~~~Main React Concepts practice~~~~~~~~~~~~~~~~~~~`
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
-}
-
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+const element = <h1>Hello, world!</h1>;
 
 
 
