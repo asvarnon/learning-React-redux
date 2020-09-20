08c580996bbffb4028f201f457630d345b887be4
Austin Varnon <austinvarnon@gmail.com>
2020-09-20 00:06:08 -0500

    working oj getting weather API to work

--- practice-react-project/src/components/Weather-forecast.js
+++ practice-react-project/src/components/Weather-forecast.js
@@ -4,17 +4,23 @@ import {Link} from 'react-router-dom';
 export default class Weather extends React.Component {
 
     state = {
-
+        loading: true
     }
 
+    async componentDidMount() {
+        const home = [-96.8084, 27.7799]; //lng, lat
+        const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${home[1]}&lon=${home[0]}&appid=${process.env.REACT_APP_WEATHER_KEY}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    }
 
 
 
     render() {
         return(
             <div>
-                <h1>Weather</h1>
-                <p>Hello there</p>
+                {this.state.loading ? <div>loading...</div> : <div>weather...</div>}
             </div>
         );
     }
