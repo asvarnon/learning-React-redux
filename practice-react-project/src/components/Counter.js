bb51e83712c2a5fe0438ad1faab9f8ec25aa5059
Austin Varnon <austinvarnon@gmail.com>
2020-09-21 19:45:46 -0500

    states understood for most part. will begin mor experimenting with them

--- practice-react-project/src/components/Counter.js
+++ practice-react-project/src/components/Counter.js
@@ -7,23 +7,33 @@ function Counter() {
     const [counter, setCounter] = useState(0)
     //above saying counter = 0,
 
+    const [toggle, setToggle] = useState(false);
+    // allows us to set a toggle as a class even
+
+    const toggler = () => {
+        //taking previous state and reversing it
+        setToggle(prevState => !prevState);
+    }
+
     const increment =() => {
         //use the given function setCounter to write out a function. prev refers to previous state.
         // the initial state is being passed as the argument for the starting point.
         setCounter(prevState => prevState+ 1);
     }
-
     const decrement = () => {
         setCounter(prevState => prevState -1);
     }
-    //TODO: @38:11
+
+
+
 
     return (
         <div className='counter'>
-            <h1>Hello</h1>
+            <h1 className={toggle ? "active" : ""}>Hello</h1>
             <h2>Counter: {counter}</h2>
             <button onClick={increment}>UP</button>
             <button onClick={decrement}>DOWN</button>
+            <button onClick={toggler}>Toggle</button>
         </div>
     )
 }
